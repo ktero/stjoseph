475cebc3725cb927bebda8f53532d50d50f7ceaf
Kenneth Tero <kennethtero748@gmail.com>
2018-02-13 19:20:53 +0800

    added new include

---
+++ include/account-section.php
@@ -0,0 +1,21 @@
+<ul class="nav navbar-top-links navbar-right">
+
+    <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+
+            <?php
+                // Display session variable fname
+                if (isset($_SESSION['id'])) 
+                    echo $name;
+             ?>
+
+                <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+        </a>
+        <ul class="dropdown-menu dropdown-user">
+            <li><a href="logout_key.php"><i class="fa fa-sign-out fa-fw"></i>Logout</a>
+            </li>
+        </ul>
+        <!-- /.dropdown-user -->
+    </li>
+    <!-- /.dropdown -->
+</ul>
