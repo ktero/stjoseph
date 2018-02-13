bdaaf3b454c31d856aebe6226d4270ff0c1df732
Kenneth Tero <kennethtero748@gmail.com>
2018-02-13 19:46:15 +0800

    update edit

--- pages/resultedit_student.php
+++ pages/resultedit_student.php
@@ -1,225 +1,172 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    session_start();
+    $name = $_SESSION['fname'];
+?>
 
-<head>
-    <?php require_once('../include/head.php') ?>
-</head>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<header style="padding: 0; margin: 0;">
-	<div id="wrapper">
-		<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+    <head>
+        <?php require_once('../include/head.php'); ?>
+    </head>
+
+    <header style="padding: 0; margin: 0;">
+        <div id="wrapper">
+            <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a> <img src="sjhs.png" style="width:40px;height:40px;"> </a> <a href="index.php">St. Joseph High School</a>
-            </div>
-            <!-- /.navbar-header -->
-
-            <ul class="nav navbar-top-links navbar-right">
-               
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                      
-                        <li><a href="adminlogin.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
-
-          <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                          <li>
-                            <a href="#">Admin Records<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">   
-                           <li>
-                                    <a href="#">Student<span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level">
-                                        <li>
-                                    <a href="addstudent.php">Add Records</a>
-                                </li>
-                                <li>
-                                    <a href="editstudent.php">Edit Records</a>
-                                </li>
-								</ul>
-								
-								
-								 <!-- /.nav-third-level -->
-								
-								<li>
-                                    <a href="#">Assessment<span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level">
-									 <li>
-                                    <a href="addassessment.php">Add Records</a>
-                                </li>
-								  <li>
-                                    <a href="editassessment2.php">Edit Records</a>
-                                </li>
-														
-								</ul>
-								</li>
-														
-								<!-- /.nav-third-level -->
-								</li>
-									 </ul>
-								 <!-- /.nav-second-level -->
-						<li>
-                            <a href="#">Student Records<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                               <li>
-                                    <a href="home.php">All Students</a>
-                                </li>
-								
-                            </ul>
-							   </li>
-                            <!-- /.nav-second-level -->
-                        <li>
-                            <a href="#">Financial Transactions<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-									<li>
-                                    <a href="formpayment.php">Student Payment</a>
-                                </li>
-								<li>
-                                    <a href="Assessment.php">Assessment</a>
-									</li>
-								
-                            </ul>
-							</li>
-                            <!-- /.nav-second-level -->
-
-                        
-                        </li>
-                    </ul>
+                    <a> <img src="sjhs.png" style="width:40px;height:40px;"> </a> <a href="index.php">St. Joseph High School</a>
                 </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
-	</div>
-</header>
+                <!-- /.navbar-header -->
+                <?php require_once('../include/account-section.php'); ?>
+                <!-- /.navbar-top-links -->
+                <?php require_once('../include/side-bar-options.php'); ?>
+                <!-- /.navbar-static-side -->
+            </nav>
+        </div>
+    </header>
 
-<body>
+    <body>
 
-    <div id="wrapper">
+        <div id="wrapper">
 
-        <!-- Navigation -->
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-				
-                    <h1 class="page-header">Edit Records</h1>
+            <!-- Navigation -->
+            <div id="page-wrapper">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Edit Records</h1>
+                        <div class="container" style="padding:200px 10px 20 px 10px; margin-bottom: 20px;">
+                            <form name="input" action="resultedit_student.php" method="post" class="form-inline">
+                                <div class="form-group">
+                                    <label for="StudentID">StudentID:</label>
+                                    <input type="text" class="form-control" id="student" name="StudentID" placeholder="eg. 20140000">
+                                    <input type='submit' class='btn btn-default' value='Search' style="background-color: lightblue">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /.col-lg-12 -->
                 </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-			
-			<!-- PHP CODE HERE -->
-			<div class="container" style="padding:200px 10px 20 px 10px">
-			<?php
-			
-			$search_value = isset($_POST['StudentID']) ?$_POST['StudentID'] : '';
-			$connection = mysqli_connect ('localhost','root','');
-			mysqli_select_db ($connection, 'sjhs');
-			
-			$query = "SELECT * FROM student WHERE StudentID LIKE '$search_value%' ";
-			$result = mysqli_query($connection, $query);
-			
-			echo "<ol>";
-				if (mysqli_num_rows($result)>0)
-				{
-					while($row = mysqli_fetch_row($result))
-					{
-						?>
-						<table border="1" cellpadding="10" width="80%">
-							<tr style="padding: 5px;">
-								<th>Student ID</th>
-								<th>Last Name</th>
-								<th>First Name</th>
-								<th>Middle Name</th>
-								<th>Gender</th>
-								<th>Address</th>
-								<th>Section</th>
-								<th>Date Enrolled</th>
-								<th>School Year</th>
-							</tr>
-							<tr class="text-center">
-								<td><?php echo $row[0]; ?></td>
-								<td><?php echo $row[1]; ?></td>
-								<td><?php echo $row[2]; ?></td>
-								<td><?php echo $row[3]; ?></td>
-								<td><?php echo $row[4]; ?></td>
-								<td><?php echo $row[5]; ?></td>
-								<td><?php echo $row[6]; ?></td>
-								<td><?php echo $row[7]; ?></td>
-								<td><?php echo $row[8]; ?></td>
-							</tr>
-							
-							<a href='edit_student.php?studentedit_key=<?php echo $row[0]; ?>' class='btn btn-default' style='background-color: lightblue'> Edit </a>
-						</table>
-						<?php
-					}
-				}
-			else
-			{
-				echo "Student Record Not Found";
-				
-			}
-			echo "</ol>";
-			mysqli_free_result($result);
-			mysqli_close($connection);
-				
-			?>
-			</div>
-			<!-- PHP CODE END -->
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                All Records
+                            </div>
+                            <div class="panel-body">
+                                <table width="100%" class="table table-striped table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Id Number</th>
+                                            <th>First Name</th>
+                                            <th>Last Name</th>
+                                            <th>Middle Name</th>
+                                            <th>Gender</th>
+                                            <th>Address</th>
+                                            <th>Year and Section</th>
+                                            <th>Date Enrolled</th>
+                                            <th>School Year</th>
+                                            <th>  </th>
+                                            <th>  </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+            <?php
+                require_once('connection.php');
+                $cn   = new connection();
+                $conn = $cn->connectDB();
 
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
+                if(isset($_POST['StudentID']))
+                    $search_value = mysqli_real_escape_string($conn, $_POST['StudentID']);
+                else
+                    echo "<meta http-equiv='refresh' content='0;url=/stjoseph/pages/resultedit_student.php' />";
 
+                $query = 'SELECT * FROM student WHERE StudentID LIKE "%'.$search_value.'%"';
+                $result = mysqli_query($conn, $query);
 
-        <!-- Footer -->
-        <footer class="text-center" style="position: absolute; padding-right: 400px; margin-bottom: 0; width: 100%; background-color: #fff; font-size: 10px">
-            <div class="row">
-                <div class="text-center">
-					<hr>
-                    <p>St. Joseph High School</p>
-					<p>Santiago St., Talakag, Bukidnon</p>
-					<p>Project Team: (Am`is, Bobadilla, Doutan, Jamero, Lapuz, Malaya, Palacios, Papa, Serra, Tabboga)</p>
-					<p>Copyright &copy; 2017</p>
+                while($row = mysqli_fetch_row($result))
+                {
+            ?>
+                                            <tr>
+                                                <td>
+                                                    <?php echo $row[0]; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $row[2]; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $row[1]; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $row[3]; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $row[4]; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $row[5]; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $row[6]; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $row[7]; ?>
+                                                </td>
+                                                <td>
+                                                    <?php echo $row[8]; ?>
+                                                </td>
+                                                <td>
+                                                    <a href='edit_student.php?studentedit_key=<?php echo $row[0]; ?>' class='btn btn-default' style='background-color: #69EC6B;'> Edit </a>
+                                                </td>
+                                                <td>
+                                                    <a href='delete_student.php?studentedit_key=<?php echo $row[0]; ?>' class='btn btn-default' style='background-color: #EA6565;'> Delete </a>
+                                                </td>
+                                            </tr>
+                <?php
+				    }
+                    $cn->closeDB();
+                ?>
+                                    </tbody>
+                                </table>
+                                <!-- /.table-responsive -->
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+
+
+                <!-- Footer -->
+                <footer class="text-center" style="position: absolute; padding-right: 400px; margin-bottom: 0; width: 100%; background-color: #fff; font-size: 10px">
+                    <div class="row">
+                        <div class="text-center">
+                            <hr>
+                            <p>St. Joseph High School</p>
+                            <p>Santiago St., Talakag, Bukidnon</p>
+                            <p>Project Team: (Am`is, Bobadilla, Doutan, Jamero, Lapuz, Malaya, Palacios, Papa, Serra, Tabboga)</p>
+                            <p>Copyright &copy; 2017</p>
+                        </div>
+                    </div>
+                </footer>
+                <!-- jQuery -->
+                <script src="../vendor/jquery/jquery.min.js"></script>
+
+                <!-- Bootstrap Core JavaScript -->
+                <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+                <!-- Metis Menu Plugin JavaScript -->
+                <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+                <!-- Custom Theme JavaScript -->
+                <script src="../dist/js/sb-admin-2.js"></script>
             </div>
-        </footer>
-    <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
+        </div>
+    </body>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
-
-</body>
-
-</html>
+    </html>
--- pages/updatestudent_form.php
+++ pages/updatestudent_form.php
@@ -1,13 +1,18 @@
+<?php
+    session_start();
+    $name = $_SESSION['fname'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <?php require_once('../include/head.php') ?>
+    <?php require_once('../include/head.php'); ?>
 </head>
 
 <header style="padding: 0; margin: 0;">
-	<div id="wrapper">
-		<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
@@ -18,100 +23,12 @@
                 <a> <img src="sjhs.png" style="width:40px;height:40px;"> </a> <a href="index.php">St. Joseph High School</a>
             </div>
             <!-- /.navbar-header -->
-
-            <ul class="nav navbar-top-links navbar-right">
-               
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                      
-                        <li><a href="adminlogin.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
+            <?php require_once('../include/account-section.php'); ?>
             <!-- /.navbar-top-links -->
-
-          <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                          <li>
-                            <a href="#">Admin Records<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">   
-                           <li>
-                                    <a href="#">Student<span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level">
-                                        <li>
-                                    <a href="addstudent.php">Add Records</a>
-                                </li>
-                                <li>
-                                    <a href="editstudent.php">Edit Records</a>
-                                </li>
-								</ul>
-								
-								
-								 <!-- /.nav-third-level -->
-								
-								<li>
-                                    <a href="#">Assessment<span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level">
-									 <li>
-                                    <a href="addassessment.php">Add Records</a>
-                                </li>
-								  <li>
-                                    <a href="editassessment2.php">Edit Records</a>
-                                </li>
-														
-								</ul>
-								</li>
-														
-								<!-- /.nav-third-level -->
-								</li>
-									 </ul>
-								 <!-- /.nav-second-level -->
-						<li>
-                            <a href="#">Student Records<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                               <li>
-                                    <a href="home.php">All Students</a>
-                                </li>
-								<li>
-                                    
-									</li>
-									<li>
-                                    
-									</li>
-								
-                            </ul>
-							   </li>
-                            <!-- /.nav-second-level -->
-                        <li>
-                            <a href="#">Financial Transactions<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-									<li>
-                                    <a href="formpayment.php">Student Payment</a>
-                                </li>
-								<li>
-                                    <a href="Assessment.php">Assessment</a>
-									</li>
-								
-                            </ul>
-							</li>
-                            <!-- /.nav-second-level -->
-
-                        
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
+            <?php require_once('../include/side-bar-options.php'); ?>
             <!-- /.navbar-static-side -->
         </nav>
-	</div>
+    </div>
 </header>
 
 <body>
@@ -119,56 +36,66 @@
     <div id="wrapper">
 
         <!-- Navigation -->
-	<!-- PHP -->
+        <!-- PHP -->
 <?php
 
-	$connection = mysqli_connect('localhost','root','');
-	mysqli_select_db($connection,'sjhs');
+	require_once('connection.php');
+    $cn = new connection();
+    $conn = $cn->connectDB();
 
+        
+	$orig_sid = isset($_POST['orig_sid']) ? mysqli_real_escape_string($conn, $_POST['orig_sid']) : '';
 	
-	$orig_sid = isset($_POST['orig_sid']) ? $_POST['orig_sid'] : '';
-	$sid = isset($_POST['sid']) ? $_POST['sid'] : '';
-	$lname = isset($_POST['lname']) ? $_POST['lname'] : '';
-	$fname = isset($_POST['fname']) ? $_POST['fname'] : '';
-	$mname = isset($_POST['mname']) ? $_POST['mname'] : '';
-	$gender = isset($_POST['gender']) ? $_POST['gender'] : '';
-	$addr = isset($_POST['addr']) ? $_POST['addr'] : '';
-	$code = isset($_POST['code']) ? $_POST['code'] : '';
-	$denrolled = isset($_POST['denrolled']) ? $_POST['denrolled'] : '';
-	$sy = isset($_POST['sy']) ? $_POST['sy'] : '';
+    $sid = isset($_POST['sid']) ? mysqli_real_escape_string($conn, $_POST['sid']) : '';
+	
+    $lname = isset($_POST['lname']) ? mysqli_real_escape_string($conn, $_POST['lname']) : '';
+        
+	$fname = isset($_POST['fname']) ? mysqli_real_escape_string($conn, $_POST['fname']) : '';
+        
+	$mname = isset($_POST['mname']) ? mysqli_real_escape_string($conn, $_POST['mname']) : '';
+        
+	$gender = isset($_POST['gender']) ? mysqli_real_escape_string($conn, $_POST['gender']) : '';
+        
+	$addr = isset($_POST['addr']) ? mysqli_real_escape_string($conn, $_POST['addr']) : '';
+        
+	$code = isset($_POST['code']) ? mysqli_real_escape_string($conn, $_POST['code']) : '';
+        
+	$denrolled = isset($_POST['denrolled']) ? mysqli_real_escape_string($conn, $_POST['denrolled']) : '';
+        
+	$sy = isset($_POST['sy']) ? mysqli_real_escape_string($conn, $_POST['sy']) : '';
 
 
 	$query = "UPDATE student SET StudentID='$sid', Lname='$lname', Fname='$fname', Mname='$mname', Gender='$gender', Address='$addr', Level_code='$code', Date_enrolled='$denrolled', School_Year='$sy' WHERE StudentID=$orig_sid";
 	
-	mysqli_query($connection, $query) or die ("Error query: " . mysqli_error($connection));
-	mysqli_close($connection);
+	mysqli_query($conn, $query) or die ("Error query: " . mysqli_error($conn));
+	$cn->closeDB();
 ?>
-<!-- Confirmation Message -->
-	<div id="page-wrapper" align="Center" style="padding:100px">
-	<h1> Successfully Updated </h1>
-	<a href='editstudent.php' class="btn btn-default" role="button" style="background-color: lightblue; text-align: right"> Update New Student</a>
-	</div>
-	 <!-- Footer -->
-        <hr>
-            <div data-role="footer" data-position="fixed" align="center">
-                    <p>St. Joseph High School</p>
-					<p>Santiago St., Talakag, Bukidnon</p>
-					<p>Project Team</p>
-					<p>(Am`is, Bobadilla, Doutan, Jamero, Lapuz, Malaya, Palacios, Papa, Serra, Tabogga)</p>
-					<p>Copyright &copy; 2017</p>
+            <!-- Confirmation Message -->
+            <div id="page-wrapper" align="Center" style="padding:100px">
+                <h1> Successfully Updated </h1>
+                <a href='editstudent.php' class="btn btn-default" role="button" style="background-color: lightblue; text-align: right"> Update New Student</a>
             </div>
-	<!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
+            <!-- Footer -->
+            <hr>
+            <div data-role="footer" data-position="fixed" align="center">
+                <p>St. Joseph High School</p>
+                <p>Santiago St., Talakag, Bukidnon</p>
+                <p>Project Team</p>
+                <p>(Am`is, Bobadilla, Doutan, Jamero, Lapuz, Malaya, Palacios, Papa, Serra, Tabogga)</p>
+                <p>Copyright &copy; 2017</p>
+            </div>
+            <!-- jQuery -->
+            <script src="../vendor/jquery/jquery.min.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+            <!-- Bootstrap Core JavaScript -->
+            <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+            <!-- Metis Menu Plugin JavaScript -->
+            <script src="../vendor/metisMenu/metisMenu.min.js"></script>
 
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
-
+            <!-- Custom Theme JavaScript -->
+            <script src="../dist/js/sb-admin-2.js"></script>
+    </div>
 </body>
 
 </html>
