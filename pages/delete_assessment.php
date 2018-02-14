a43617e93abb68545d5553d9fc9280e7c592a09b
Kenneth Tero <kennethtero748@gmail.com>
2018-02-14 22:14:30 +0800

    added delete_assessment

---
+++ pages/delete_assessment.php
@@ -0,0 +1,13 @@
+<?php
+    
+    require_once('connection.php');
+    $cn = new connection();
+    $conn = $cn->connectDB();
+    
+    $edit_key = isset($_GET['Fee_code']) ? $_GET['studentedit_key'] : '';
+    $query= "DELETE FROM fees WHERE Fee_code='".$edit_key."'";
+    mysqli_query($conn, $query);
+
+    $cn->closeDB();
+    header('Location:resultedit_assessment.php');
+?>
