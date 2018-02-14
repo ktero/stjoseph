3f1489accfd57a41489677d8db35d8130f1d7980
Kenneth Tero <kennethtero748@gmail.com>
2018-02-14 20:31:58 +0800

    added footer

---
+++ include/footer.php
@@ -0,0 +1,11 @@
+<footer class="text-center" style="position: fixed; bottom: 0; width: 100%; background-color: #fff; font-size: 10px">
+    <div class="row">
+        <div class="text-center">
+            <hr>
+            <p>St. Joseph High School</p>
+            <p>Santiago St., Talakag, Bukidnon</p>
+            <p>Project Team: (Am`is, Bobadilla, Doutan, Jamero, Lapuz, Malaya, Palacios, Papa, Serra, Tabboga)</p>
+            <p>Copyright &copy; 2017</p>
+        </div>
+    </div>
+</footer>
