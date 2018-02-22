5765e1d88d5d435b6b45eb917521425701b5ea19
Jaaamrod <36471122+Jaaamrod@users.noreply.github.com>
2018-02-22 17:03:38 +0800

    Added 2sessionstart
    1st sessions is for the picking of the database, 2nd sessions is for the allowing to use the database

--- include/account-section.php
+++ include/account-section.php
@@ -1,4 +1,5 @@
 <ul class="nav navbar-top-links navbar-right">
+
     <li class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" href="#">
 
@@ -7,6 +8,7 @@
                 if (isset($_SESSION['id']))
                     echo $user;
              ?>
+
                 <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
         </a>
         <ul class="dropdown-menu dropdown-user">
