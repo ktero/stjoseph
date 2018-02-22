0ebd9355966431b4aa2c6200f1134a03a807c67f
Kenneth Tero <kennethtero748@gmail.com>
2018-02-22 15:57:30 +0800

    updated index.php - background image

--- pages/index.php
+++ pages/index.php
@@ -2,7 +2,6 @@
    require_once('../include/sessionstart.php');
 ?>
 
-
     <!DOCTYPE html>
     <html lang="en">
 
@@ -13,24 +12,8 @@
     <body>
 
         <div id="wrapper">
-
             <!-- Navigation -->
-            <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                    <a> <img src="sjhs.png" style="width:40px;height:40px;"> </a> <a href="index.php">Saint Joseph High School of Talakag</a>
-                </div>
-                <!-- /.navbar-header -->
-                <?php require_once('../include/account-section.php'); ?>
-                <!-- /.navbar-top-links -->
-                <?php require_once('../include/side-bar-options.php'); ?>
-                <!-- /.navbar-static-side -->
-            </nav>
+            <?php require_once('../include/navdiv-title.php'); ?>
         </div>
         <!-- /#wrapper -->
 
@@ -38,7 +21,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <center>
-                      <img src="graphics/logo_sjhs.png" alt="st.joseph-logo" />
+                      <img src="graphics/logo_sjhs50.png" alt="st.joseph-logo" />
                     </center>
                 </div>
                 <!-- /.col-lg-12 -->
