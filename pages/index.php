eeef3981c5270f862922787a32f099fec1bbcbe3
Jaaamrod <36471122+Jaaamrod@users.noreply.github.com>
2018-02-22 13:09:45 +0800

    Added Switcher and Creator
    Switcher - switching database to use
    Creator- to create duplicate database for the next school year

--- pages/index.php
+++ pages/index.php
@@ -2,6 +2,7 @@
    require_once('../include/sessionstart.php');
 ?>
 
+
     <!DOCTYPE html>
     <html lang="en">
 
@@ -9,55 +10,61 @@
         <?php require_once('../include/head.php'); ?>
     </head>
 
-    <header style="padding: 0; margin: 0;">
-        <div id="wrapper">
-            <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                  <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                    <a> <img src="sjhs.png" style="width:40px;height:40px;"> </a> <a href="index.php">Saint Joseph High School of Talakag</a>
-                </div>
-                <!-- /.navbar-header -->
-                <?php require_once('../include/account-section.php'); ?>
-                <!-- /.navbar-top-links -->
-                <?php require_once('../include/side-bar-options.php'); ?>
-                <!-- /.navbar-static-side -->
-            </nav>
-        </div>
-    </header>
-
-    <body style="padding-bottom: 80px; background: url(sjhs.jpg) no-repeat; background-size: 100% 355%; background-position: center 40px;">
+    <body>
 
         <div id="wrapper">
+
+            <!-- Navigation -->
+            <?php require_once('../include/navdiv-title.php'); ?>
         </div>
         <!-- /#wrapper -->
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <center>
+                      <img src="graphics/logo_sjhs.png" alt="st.joseph-logo" />
+                    </center>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#wrapper -->
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <?php require_once('../include/footer.php'); ?>
+            </div>
+        </footer>
+        <!-- jQuery -->
+        <script src="../vendor/jquery/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+        <!-- DataTables JavaScript -->
+        <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
+        <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+        <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="../dist/js/sb-admin-2.js"></script>
+
+        <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+        <script>
+            $(document).ready(function() {
+                $('#dataTables-example').DataTable({
+                    responsive: true
+                });
+            });
+        </script>
+
     </body>
-    <!-- Footer -->
-    <footer class="text-center" style="position: absolute; bottom: 0; width: 100%; background-color: #fff; font-size: 10px">
-        <?php require_once('../include/footer.php'); ?>
-    </footer>
-
-
-
-    <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="../vendor/raphael/raphael.min.js"></script>
-    <script src="../vendor/morrisjs/morris.min.js"></script>
-    <script src="../data/morris-data.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
 
     </html>
