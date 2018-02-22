d2369147a9dbfd786b40f267952f174c68cb6470
Kenneth Tero <kennethtero748@gmail.com>
2018-02-22 20:15:18 +0800

    remove files

--- include/dbchecker.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	    if(isset($_POST['input'])){
-            $_SESSION['database']=$_POST['input'];
-            header('Location: ../pages/index.php');
-        }
-        else
-        	header('Location: ../pages/switcher.php');
-?>
