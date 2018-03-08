a5c40394353eb087085db5dde5db0b13fe8315fb
Kenneth Tero <kennethter748@gmail.com>
2018-03-08 17:42:57 +0800

    removed files - switcher, creator, creator1, dbchecker, and others.

--- include/sessionstart2.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    // Session validation
-    if(!isset($_SESSION['username']))
-        header('Location: login.php');
-    else
-    	$_SESSION['database'] = NULL;
-?>
