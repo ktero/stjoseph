86da87ec00cf33fb89d7e8f7977cb6dfa65386c1
Kenneth Tero <kennethter748@gmail.com>
2018-03-20 23:53:29 +0800

    updated backup path

--- pages/logout_key.php
+++ pages/logout_key.php
@@ -3,9 +3,9 @@
     require_once('connection.php');
     $cn   = new connection();
     $conn = $cn->connectDB();
-    
+
     $dbName = $cn->getDatabaseName(); // Retrive database name
-    exec("D:/xampp/mysql/bin/mysqldump -u root ".$dbName." > D:/xampp/htdocs/stjoseph/databasebackup/".$dbName."_backup.sql");
+    exec("C:/xampp/mysql/bin/mysqldump -u root ".$dbName." > C:/xampp/htdocs/stjoseph/databasebackup/".$dbName."_backup.sql");
 
     session_destroy();
     header('Location: login.php');
