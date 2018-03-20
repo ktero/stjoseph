f2aba25a7ed7f1ec56bcd7f91ee36933f15d0de5
Kenneth Tero <kennethter748@gmail.com>
2018-03-20 19:19:05 +0800

    added system-description

---
+++ include/system-description.php
@@ -0,0 +1,15 @@
+<!--
+CSLP Project: St. Joseph High School of Talakag Profiling and Financial System.
+Brief Description: A profiling and financial system that track student records and transactions.
+
+Developers:
+- Jamrod Buat
+- Philip Endiape
+- Marvin Fuentes (Project leader)
+- Justine Fumar
+- Kenneth Tero
+
+Contact our project leader for more information of our project: marvinfuentes99@gmail.com
+
+Copyright © 2018 All rights reserved.
+-->
