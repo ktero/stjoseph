b9ebb72a9b377cd6b0870fda0c6f3194a3eda793
Kenneth <ktero.dev@gmail.com>
2019-06-17 20:23:15 +0800

    changes made

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,6 @@
 <div class="col-lg-12 text-center">
   <h5>St. Joseph High School Santiago Street, Talakag, Bukidnon</h5>
-  <h5>Project Teams:</h5>
-  <p>Information System: (Am`is, Bobadilla, Doutan, Jamero, Lapuz, Malaya, Palacios, Papa, Serra, Tabboga)</p>
-  <p>Computer Science: (Buat, Endiape, Fuentes, Fumar, Tero)</p>
+  <h5>Project Team:</h5>
+  <p>Jamrod Buat, Philip Endiape, Marvin Fuentes, Justine Fumar, Kenneth Tero</p>
   <p>Copyright &copy; 2018</p>
 </div>
